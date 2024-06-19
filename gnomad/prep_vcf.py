7279b80aee5d5afd4c75089c25297e5eeeb17dea
Sara Mathieson <sara.k.mathieson@gmail.com>
2024-06-19 16:13:36 -0400

    prep vcf outgroup

--- gnomad/prep_vcf.py
+++ gnomad/prep_vcf.py
@@ -27,31 +27,32 @@ def main():
 
     # go through each population
     for pop_file in pop_file_lst:
-        pop = pop_file[:3].upper()
-        pop_file = "gnomad_subpops/" + pop_file
+        if "_yri.txt" in pop_file:
+            pop = pop_file[:3].upper()
+            pop_file = "gnomad_subpops/" + pop_file
 
-        # folder for each pop
-        out_folder = IN_FOLDER + "/" + pop
-        mkdir = "mkdir " + out_folder
-        process = Popen(mkdir, shell=True, stdout=PIPE)
-        process.communicate()
-
-        # go through each chromosome
-        for i in range(1,23):
-            chr = str(i)
-            in_file = IN_FOLDER + "/hgdp1kgp_chr" + chr + ".filtered.SNV_INDEL.phased.shapeit5.bcf"
-            out_file = out_folder + "/" + pop + "_chr" + chr + ".vcf.gz"
-            
-            view_norm = "bcftools view -S " + pop_file + " --force-samples --min-ac 1:minor -m2 -M2 -v snps -Ou " + in_file + " | bcftools norm -d snps -Oz -o " + out_file
-            print(view_norm)
-            process = Popen(view_norm, shell=True, stdout=PIPE)
+            # folder for each pop
+            out_folder = IN_FOLDER + "/" + pop
+            mkdir = "mkdir " + out_folder
+            process = Popen(mkdir, shell=True, stdout=PIPE)
             process.communicate()
 
-            # index
-            index = "bcftools index " + out_folder + "/" + pop + "_chr" + chr + ".vcf.gz"
-            print(index)
-            process = Popen(index, shell=True, stdout=PIPE)
-            process.communicate()
+            # go through each chromosome
+            for i in range(1,23):
+                chr = str(i)
+                in_file = IN_FOLDER + "/hgdp1kgp_chr" + chr + ".filtered.SNV_INDEL.phased.shapeit5.bcf"
+                out_file = out_folder + "/" + pop + "_chr" + chr + ".vcf.gz"
+                
+                view_norm = "bcftools view -S " + pop_file + " --force-samples --min-ac 1:minor -m2 -M2 -v snps -Ou " + in_file + " | bcftools norm -d snps -Oz -o " + out_file
+                print(view_norm)
+                process = Popen(view_norm, shell=True, stdout=PIPE)
+                process.communicate()
+
+                # index
+                index = "bcftools index " + out_folder + "/" + pop + "_chr" + chr + ".vcf.gz"
+                print(index)
+                process = Popen(index, shell=True, stdout=PIPE)
+                process.communicate()
 
 if __name__ == "__main__":
     main()
