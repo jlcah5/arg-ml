854c531452008ad744e75f9a7e30370c377fd9fc
Sara Mathieson <smathieson@haverford.edu>
2024-07-22 12:32:23 -0400

    extend to all chroms

--- gnomad/gnomad_relate.py
+++ gnomad/gnomad_relate.py
@@ -20,11 +20,8 @@ import real_data_random
 # GLOBALS
 ################################################################################
 
-# TODO expand to all chroms and pops
-CHR = "3"
-POP = "ACB"
-
 IN_FOLDER = sys.argv[1]
+POP = IN_FOLDER[-3:]
 BED_FILE = sys.argv[2]
 RECO_FOLDER = sys.argv[3] + "/" + POP
 OUT_FOLDER = sys.argv[4] + "/" + POP
@@ -55,68 +52,71 @@ def main():
 
     # length of chrom
     chrom_dict = read_chrom_lengths()
-    chrom_length = chrom_dict[CHR]
 
     # accessibility mask
     mask_dict = real_data_random.read_mask(BED_FILE)
-
-    # go through each region
-    start = 0
-    end = WINDOW
     kept = 0
     total = 0
-    while end <= chrom_length:
-        total += 1
 
-        cmd = "bcftools view --no-header -r chr" + CHR + ":" + str(start) + "-" + str(end) + " " + IN_FOLDER + "/" + POP + "/" + POP + "_chr" + CHR + ".vcf.gz | wc -l"
-        process = Popen(cmd, shell=True, stdout=PIPE)
-        output, err = process.communicate()
-        num_snps = int(output.decode("utf-8"))
+    for CHR in chrom_dict:
+        chrom_length = chrom_dict[CHR]
 
-        # create region to determine accessibility
-        region = real_data_random.Region(CHR, start, end)
+        # go through each region
+        start = 0
+        end = WINDOW
         
-        # if we have enough SNPs and inside accessibility mask
-        if num_snps >= MIN_SNPS and region.inside_mask(mask_dict):
-            kept += 1
+        while end <= chrom_length:
+            total += 1
 
-            print("num SNPs", num_snps)
-            prefix = POP + "_chr" + CHR + "_" + str(start) + "_" + str(end)
+            cmd = "bcftools view --no-header -r chr" + CHR + ":" + str(start) + "-" + str(end) + " " + IN_FOLDER + "/" + POP + "_chr" + CHR + ".vcf.gz | wc -l"
+            process = Popen(cmd, shell=True, stdout=PIPE)
+            output, err = process.communicate()
+            num_snps = int(output.decode("utf-8"))
 
-            # extract SNPs
-            bcftools_cmd = "bcftools view -r chr" + CHR + ":" + str(start) + "-" + str(end) + " -Oz -o " + prefix + ".vcf.gz " + IN_FOLDER + "/" + POP + "/" + POP + "_chr" + CHR + ".vcf.gz"
-            process = Popen(bcftools_cmd, shell=True, stdout=PIPE)
-            process.communicate() # wait to finish
-
-            # convert from vcf.gz to haps and sample
-            relate = "RelateFileFormats --mode ConvertFromVcf --haps " + prefix + ".haps --sample " + prefix + ".sample -i " + prefix
-            process = Popen(relate, shell=True, stdout=PIPE)
-            process.communicate() # wait to finish
-
-            # infer tree (biallelic snps retained with bcftools)
-            # use population specific genetic map from Spence and Song
-            reco_file = RECO_FOLDER + "/" + POP + "_recombination_map_hapmap_format_hg38_chr_" + CHR + ".txt"
-            relate = "Relate --mode All -m 1.25e-8 -N 20000 --haps " + prefix + ".haps --sample " + prefix + ".sample --map " + reco_file + " --seed 1 -o " + prefix
-            process = Popen(relate, shell=True, stdout=PIPE)
-            process.communicate() # wait to finish
+            # create region to determine accessibility
+            region = real_data_random.Region(CHR, start, end)
             
-            # convert to tree sequence
-            relate = "RelateFileFormats --mode ConvertToTreeSequence -i " + prefix + " -o " + prefix + ".infer"
-            process = Popen(relate, shell=True, stdout=PIPE)
-            process.communicate() # wait to finish
+            # if we have enough SNPs and inside accessibility mask
+            if num_snps >= MIN_SNPS and region.inside_mask(mask_dict):
+                kept += 1
 
-            # calculate GRM on entire region
-            egrm = "trees2egrm --output-format numpy " + prefix + ".infer.trees --c --haploid --output " + OUT_FOLDER + "/" + prefix
-            process = Popen(egrm, shell=True, stdout=PIPE)
-            process.communicate() # wait to finish
-            
-            # clean 
-            clean = "rm -rf " + prefix + "*"
-            process = Popen(clean, shell=True, stdout=PIPE)
-            process.communicate() # wait to finish
+                print("num SNPs", num_snps)
+                prefix = POP + "_chr" + CHR + "_" + str(start) + "_" + str(end)
 
-        start += STEP
-        end += STEP
+                # extract SNPs
+                bcftools_cmd = "bcftools view -r chr" + CHR + ":" + str(start) + "-" + str(end) + " -Oz -o " + prefix + ".vcf.gz " + IN_FOLDER + "/" + POP + "_chr" + CHR + ".vcf.gz"
+                process = Popen(bcftools_cmd, shell=True, stdout=PIPE)
+                process.communicate() # wait to finish
+
+                # convert from vcf.gz to haps and sample
+                relate = "RelateFileFormats --mode ConvertFromVcf --haps " + prefix + ".haps --sample " + prefix + ".sample -i " + prefix
+                process = Popen(relate, shell=True, stdout=PIPE)
+                process.communicate() # wait to finish
+
+                # infer tree (biallelic snps retained with bcftools)
+                # use population specific genetic map from Spence and Song
+                reco_file = RECO_FOLDER + "/" + POP + "_recombination_map_hapmap_format_hg38_chr_" + CHR + ".txt"
+                relate = "Relate --mode All -m 1.25e-8 -N 20000 --haps " + prefix + ".haps --sample " + prefix + ".sample --map " + reco_file + " --seed 1 -o " + prefix
+                process = Popen(relate, shell=True, stdout=PIPE)
+                process.communicate() # wait to finish
+                
+                # convert to tree sequence
+                relate = "RelateFileFormats --mode ConvertToTreeSequence -i " + prefix + " -o " + prefix + ".infer"
+                process = Popen(relate, shell=True, stdout=PIPE)
+                process.communicate() # wait to finish
+
+                # calculate GRM on entire region
+                egrm = "trees2egrm --output-format numpy " + prefix + ".infer.trees --c --haploid --output " + OUT_FOLDER + "/" + prefix
+                process = Popen(egrm, shell=True, stdout=PIPE)
+                process.communicate() # wait to finish
+                
+                # clean 
+                clean = "rm -rf " + prefix + "*"
+                process = Popen(clean, shell=True, stdout=PIPE)
+                process.communicate() # wait to finish
+
+            start += STEP
+            end += STEP
 
     print("frac kept", kept/total)
 
